6bae9d9e28a46fad8912b17a067f6ecdc53109d2
giuliopaci@gmail.com <giuliopaci@gmail.com@4622ca98-d053-0410-b680-d1345939a8d7>
2013-01-08 22:34:37 +0000

    Improved BitOps.h.
     * __fls has been renamed to find_last_bit_set
     * the x86 assembly implementation of find_last_bit_set is not selected
       by default
     * the generic implementation of find_last_bit_set supports 64bit
     * __builtin_clzl can be used to implement find_last_bit_set

--- trunk/src/util/BitOps.h
+++ trunk/src/util/BitOps.h
@@ -35,14 +35,12 @@
 #ifndef BITOPS_H
 #define BITOPS_H
 
-//#define ASM_GENERIC  // Uncomment to use CPU independent implementation.
-
 ///////////////////////////////////////////////////////////////////////////////
 
 // Find the last (most-significant) bit set.
 // Note fls(0) = 0, fls(1) = 1, fls(0x80000000) = 32.
-static __inline__ unsigned long __fls(unsigned long x) {
-#ifndef ASM_GENERIC
+static __inline__ unsigned long find_last_bit_set(unsigned long x) {
+#ifdef HAVE_X86_ASM
     #if __WORDSIZE == 64
         if (!x) return 0;
         __asm__("bsrq %1,%0"
@@ -56,19 +54,34 @@ static __inline__ unsigned long __fls(unsigned long x) {
                 "movl $-1,%0\n"
                 "1:"
                 :"=r" (r)
-                :"rm" ((int)x));
+                :"rm" (x));
         return r + 1;
     #endif
 #else
-    // TODO: Add 64-bit support.
-    int r = 32;
-    if (!x)  return 0;
-    if (!(x & 0xffff0000u)) { x <<= 16; r -= 16; }
-    if (!(x & 0xff000000u)) { x <<= 8;  r -= 8;  }
-    if (!(x & 0xf0000000u)) { x <<= 4;  r -= 4;  }
-    if (!(x & 0xc0000000u)) { x <<= 2;  r -= 2;  }
-    if (!(x & 0x80000000u)) { x <<= 1;  r -= 1;  }
-    return r;
+#ifdef HAVE___BUILTIN_CLZ
+return (x? (((sizeof(unsigned long)*8))- __builtin_clzl(x)): 0);
+#else
+    #if __WORDSIZE == 64
+	int r = 64;
+	if (!x)  return 0;
+	if (!(x & 0xffffffff00000000ul)) { x <<= 32; r -= 32; }
+	if (!(x & 0xffff000000000000ul)) { x <<= 16; r -= 16; }
+	if (!(x & 0xff00000000000000ul)) { x <<= 8;  r -= 8;  }
+	if (!(x & 0xf000000000000000ul)) { x <<= 4;  r -= 4;  }
+	if (!(x & 0xc000000000000000ul)) { x <<= 2;  r -= 2;  }
+	if (!(x & 0x8000000000000000ul)) { x <<= 1;  r -= 1;  }
+	return r;
+    #else
+	int r = 32;
+	if (!x)  return 0;
+	if (!(x & 0xffff0000ul)) { x <<= 16; r -= 16; }
+	if (!(x & 0xff000000ul)) { x <<= 8;  r -= 8;  }
+	if (!(x & 0xf0000000ul)) { x <<= 4;  r -= 4;  }
+	if (!(x & 0xc0000000ul)) { x <<= 2;  r -= 2;  }
+	if (!(x & 0x80000000ul)) { x <<= 1;  r -= 1;  }
+	return r;
+    #endif
+#endif
 #endif
 }
 
@@ -78,6 +91,6 @@ static __inline__ bool isPowerOf2(unsigned long x)
 
 // Returns the smallest power of 2 larger than x.
 static __inline__ unsigned long nextPowerOf2(unsigned long x)
-{ return 1UL << __fls(x); }
+{ return 1UL << find_last_bit_set(x); }
 
 #endif // BITOPS_H
