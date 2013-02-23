cd72940e37c74052a33510eddb2bdf06e427a23a
giuliopaci@gmail.com <giuliopaci@gmail.com@4622ca98-d053-0410-b680-d1345939a8d7>
2013-02-23 22:57:35 +0000

    Updated copyright information for several files.

--- trunk/src/util/ZFile.h
+++ trunk/src/util/ZFile.h
@@ -1,5 +1,7 @@
 ////////////////////////////////////////////////////////////////////////////
 // Copyright (c) 2008, Massachusetts Institute of Technology              //
+// Copyright (c) 2010-2013, Giulio Paci <giuliopaci@gmail.com>            //
+// Copyright (c) 2013, Jakub Wilk  <jwilk@debian.org>                     //
 // All rights reserved.                                                   //
 //                                                                        //
 // Redistribution and use in source and binary forms, with or without     //
