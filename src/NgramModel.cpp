2e04daa9dfb5d9e232ae6e2eaf8889a422e82372
giuliopaci <giuliopaci@gmail.com>
2013-02-23 22:57:35 +0000

    Updated copyright information for several files.

--- src/NgramModel.cpp
+++ src/NgramModel.cpp
@@ -1,5 +1,6 @@
 ////////////////////////////////////////////////////////////////////////////
 // Copyright (c) 2008, Massachusetts Institute of Technology              //
+// Copyright (c) 2010-2013, Giulio Paci <giuliopaci@gmail.com>            //
 // All rights reserved.                                                   //
 //                                                                        //
 // Redistribution and use in source and binary forms, with or without     //
