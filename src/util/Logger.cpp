orezpraw/MIT-Language-Modeling-Toolkit

Made all logs go to stderr.

src/util/Logger.cpp
@@ -52,9 +52,12 @@ void Logger::Log(int level, const char *fmt, ...) {
         va_list args;
         va_start(args, fmt);
         if (_timestamp) 
-            printf("%.3f\t", (float)(clock() - _startTime) / CLOCKS_PER_SEC);
-        vprintf(fmt, args);
+            fprintf(stderr, "%.3f\t", (float)(clock() - _startTime) / CLOCKS_PER_SEC);
+        fprintf(stderr, "\e[0;36m");
+        vfprintf(stderr, fmt, args);
         va_end(args);
+        fprintf(stderr, "\e[m");
+        fflush(stderr);
     }
 }
 
@@ -68,6 +71,7 @@ void Logger::Warn(int level, const char *fmt, ...) {
         vfprintf(stderr, fmt, args);
         fprintf(stderr, "\e[m");
         va_end(args);
+        fflush(stderr);
     }
 }
 
@@ -81,5 +85,6 @@ void Logger::Error(int level, const char *fmt, ...) {
         vfprintf(stderr, fmt, args);
         fprintf(stderr, "\e[m");
         va_end(args);
+        fflush(stderr);
     }
 }

mitlm/mitlm

src/util/Logger.cpp
@@ -49,17 +49,17 @@ namespace mitlm {
 #endif
 clock_t Logger::_startTime = clock();
 FILE*   Logger::_err_file = stderr;
-FILE*   Logger::_out_file = stdout;
 
 void Logger::Log(int level, const char *fmt, ...) {
     if (_verbosity >= level) {
-	if ( _out_file != NULL ) {
+	if ( _err_file != NULL ) {
             va_list args;
             va_start(args, fmt);
             if (_timestamp) 
-	        fprintf(_out_file, "%.3f\t", (double)(clock() - _startTime) / CLOCKS_PER_SEC);
-            vfprintf(_out_file, fmt, args);
+	        fprintf(_err_file, "%.3f\t", (double)(clock() - _startTime) / CLOCKS_PER_SEC);
+            vfprintf(_err_file, fmt, args);
             va_end(args);
+            fflush(_err_file);
 	}
     }
 }
@@ -70,15 +70,12 @@ void Logger::Warn(int level, const char *fmt, ...) {
 	if ( _err_file != NULL ) {
             va_start(args, fmt);
             fprintf(_err_file, "\033[0;33m");
-	}
-	if ( _out_file != NULL ) {
             if (_timestamp) 
-                fprintf(_out_file, "%.3f\t", (double)(clock() - _startTime) / CLOCKS_PER_SEC);
-	}
-	if ( _err_file != NULL ) {
+                fprintf(_err_file, "%.3f\t", (double)(clock() - _startTime) / CLOCKS_PER_SEC);
             vfprintf(_err_file, fmt, args);
             fprintf(_err_file, "\033[m");
             va_end(args);
+            fflush(_err_file);
 	}
     }
 }
@@ -89,15 +86,12 @@ void Logger::Error(int level, const char *fmt, ...) {
 	if ( _err_file != NULL ) {
             va_start(args, fmt);
             fprintf(_err_file, "\033[1;31m");
-	}
-	if ( _out_file != NULL ) {
             if (_timestamp) 
-                fprintf(_out_file, "%.3f\t", (double)(clock() - _startTime) / CLOCKS_PER_SEC);
-	}
-	if ( _err_file != NULL ) {
+                fprintf(_err_file, "%.3f\t", (double)(clock() - _startTime) / CLOCKS_PER_SEC);
             vfprintf(_err_file, fmt, args);
             fprintf(_err_file, "\033[m");
             va_end(args);
+            fflush(_err_file);
 	}
     }
 }